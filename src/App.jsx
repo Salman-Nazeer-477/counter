079be8321f8884c941307be91ff433d2cd7cb398
Salman Nazeer <salmannazeer477@gmail.com>
2025-12-16 19:41:46 +0530

    changed simple counter into a prayer counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import { useEffect, useState } from "react";
+import Prayer from './Prayer'
 import './App.css'
 
 
-function App(){
-  const [count, setCount] = useState(() => {
-    const localValue = localStorage.getItem("count")
-    if(localValue === null) return 0
-    return Number(localValue)
-  })
-
-  useEffect(() => {
-    localStorage.setItem("count", String(count))
-  }, [count])
-
-  function incrementCount(){
-    setCount(count => {
-      return count + 1
-    })
-  }
-
-  function decrementCount(){
-    setCount(count => {
-      if(count == 0) return 0
-      return count - 1
-    })
-  }
+function App() {
 
   return (
     <>
-      <div>
-        <h1>{ count }</h1>
-      </div>
-      <div>
-        <button onClick={ decrementCount }>-</button>
-        <button onClick={ incrementCount }>+</button>
-      </div>
+      <Prayer prayerText="Subhanallah" />
+      <Prayer prayerText="Alhamdulillah" />
+      <Prayer prayerText="Allahu Akbar" />
+      <Prayer prayerText="La ilaha illallah" />
     </>
   )
 }
---
+++ src/Prayer.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function Prayer(props){
+    const [count, setCount] = useState(() => {
+        const localValue = localStorage.getItem(`${props.prayerText}`)
+        if(localValue === null) return 0
+        return Number(localValue)
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(`${props.prayerText}`, String(count))
+    }, [count])
+
+    return (
+        <>
+            <div className="prayer-component">
+                <span className="prayer-content">
+                    <p className="prayer-text">{props.prayerText}</p>
+                    <button onClick={() => setCount(count + 1)}>{count}</button>
+                </span>
+            </div>
+        </>
+    )
+}
